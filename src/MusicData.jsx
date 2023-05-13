4c9e264ad106da82942f51534474fc32f0bc3498
ritikanubhav <ritikanubhav59@gmail.com>
2023-05-14 01:20:34 +0530

    initial commit

---
+++ src/MusicData.jsx
@@ -0,0 +1,37 @@
+export default[
+    {
+        title:"DARU BADNAAM KARTI",
+        author:"Kamal Kahlon",
+        duration:186,
+        url:"./songs/Daru Badnaam.m4a",
+        img:"./images/daru badnam.jpg"
+    },
+    {
+        title:"O SUDH BUDH KHOYI",
+        author:"Arijit Singh",
+        duration:261,
+        url:"./songs/O sudh budh khoyi hai khoyi Maine.m4a",
+        img:"./images/o sudh budh.jpeg"
+    },
+    {
+        title:"RANG JO LAGYO",
+        author:"Atif Aslam",
+        duration:129,
+        url:"./songs/Rang Jo Lagyo.m4a",
+        img:"./images/Rang jo lagyo.jpeg"
+    },
+    {
+        title:"SAMAY SAMJHAYEGA",
+        author:"Mohit Lalwani",
+        duration:198,
+        url:"./songs/Samay Samjhayega.m4a",
+        img:"./images/samay samjhayega.jpg"
+    },
+    {
+        title:"SOCHA HAI",
+        author:"Jubin Nautiyal",
+        duration:136,
+        url:"./songs/Socha Hai Song.m4a",
+        img:"./images/socha hai.jpeg"
+    }
+]
---
+++ src/components/Player.jsx
@@ -0,0 +1,161 @@
+import React,{useState,useEffect,useRef} from 'react';
+import {BsPlayFill,BsPauseFill,BsShuffle,BsSkipEndFill,BsSkipStartFill} from "react-icons/bs";
+import {ImLoop} from "react-icons/im";
+
+export default function Player(props)
+{
+    const [active,setActive]=useState({
+        isLoop:false,
+        isPlaying:false,
+    })
+
+    console.log("player rendered",active.isPlaying)
+
+    const [currentTime,setCurrentTime]=useState(0)
+    const audioEl=useRef(null)
+    const duration=props.music[props.index].duration
+    useEffect(()=>{
+        console.log("time updated",audioEl.current.currentTime)
+        function watchtime(){
+            setCurrentTime(audioEl.current.currentTime)
+        }
+        audioEl.current.addEventListener("timeupdate",watchtime)
+        return function(){
+            audioEl.current.removeEventListener("timeupdate",watchtime)
+        }
+    },[currentTime])
+    
+    function changeHandler(event){
+        audioEl.current.currentTime=event.target.value*audioEl.current.duration/1000;
+    }
+
+    useEffect(()=>{
+        console.log("isPlaying render")
+        if(active.isPlaying)
+        {
+            audioEl.current.play()
+            document.getElementById("cover-img").style.animationPlayState = "running";
+        }  
+        else{
+            audioEl.current.pause()
+            document.getElementById("cover-img").style.animationPlayState = "paused";
+        }
+    })
+
+    useEffect(()=>{
+        console.log("loop render")
+        if(active.isLoop)
+        {
+            audioEl.current.loop=true;
+        }  
+        else{
+            audioEl.current.loop=false;
+        }
+    },[active.isLoop])
+
+    function skipSong(forward = true)
+    {
+        console.log("skipSong",props.index)
+        setCurrentTime(0)
+        audioEl.current.currentTime=0;
+        if(forward)
+        {
+            props.setIndex((temp)=>{
+              return ((temp+1)%(props.music.length))
+            })
+        }
+        else{
+            props.setIndex(()=>{
+                let temp=props.index
+                temp--
+                if(temp<0)
+                {
+                    temp=props.music.length-1
+                }
+                return temp
+            })   
+        }
+    }
+
+    function loopHandler(){
+        
+            setActive(function(prev){
+                return({
+                    ...prev,
+                    isLoop:!prev.isLoop
+                })
+            })
+            console.log("loop clicked",active.isLoop)
+    }
+
+    function clickHandler(){ 
+        setActive(function(prev){
+            return({
+                ...prev,
+                isPlaying:!prev.isPlaying
+            })
+        })
+        console.log(" play clicked",active.isPlaying)
+    }
+
+    function shuffleHandler(event){
+        const random=Math.floor(Math.random() * props.music.length)
+        props.setIndex(()=>random)
+        console.log("shuffle clicked")
+    }
+
+    function formatter(myNumber){
+        myNumber=Math.floor(myNumber%60)
+        return ("0" + myNumber).slice(-2);
+    }
+
+    return(
+        <div className="card">
+
+            <audio 
+            src={props.music[props.index].url} 
+            ref={audioEl} preload="auto" 
+            onEnded={()=>skipSong(true)}
+            />
+
+            <div className="cd">
+                <img 
+                id="cover-img" 
+                src={props.music[props.index].img}
+                />
+                <div className="circle"></div>
+            </div>
+
+            <h4 className="title">
+            {props.music[props.index].title}
+            </h4>
+
+            <p className="author">
+            {props.music[props.index].author}
+            </p>
+
+            <input 
+            min="1" 
+            max="1000" 
+            className="slider" 
+            type="range" 
+            value={Math.floor((currentTime/duration)*1000)} 
+            onChange={changeHandler} />
+
+            <div className="time">
+                <p>{Math.floor(currentTime/60)}:{formatter(currentTime)}</p>
+                <p>{Math.floor(duration/60)}:{formatter(duration)}</p>
+            </div>
+            <div className="button-container">
+                <BsShuffle   id="shuffle" onClick={shuffleHandler}/>
+                <BsSkipStartFill  className="button"  onClick={()=>skipSong(false)}/>
+                {!(active.isPlaying) ?
+                 <BsPlayFill  className="button" onClick={clickHandler} />
+                : <BsPauseFill  className="button" onClick={clickHandler} />
+                }
+                <BsSkipEndFill  className="button" onClick={()=>skipSong(true)}/>
+                <ImLoop className={active.isLoop && "loopActive"} onClick={loopHandler}/>
+            </div>
+        </div>
+    )
+}
